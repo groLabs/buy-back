b17c45de0e75d9a2bc9b736d320082558b2bcb68
kitty-the-kat <kristian@gro.xyz>
2022-11-13 08:01:44 +0000

    [setup] KD - 13.11.22 - adding mapping and removing default test

---
+++ test/buyBack.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract buyBackTest is Test {
+
+    function setUp() public {
+    }
+
+    function testIncrement() public {
+    }
+
+    function testSetNumber(uint256 x) public {
+    }
+}
